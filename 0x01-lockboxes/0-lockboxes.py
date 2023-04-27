b1a05df26f2b10bd6796ba3d6949c1df0261543e
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2023-04-27 23:01:46 +0100

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
+"""Defines a function that determines if a box containing a list
+   of lists can be opened using keys stored in the lists.
+"""
+
+
 def canUnlockAll(boxes):
-    """Determines if all boxes can be opened"""
-    n = len(boxes)
-    unlocked = [False] * n
-    unlocked[0] = True
-    stack = [0]
+    """Determines if boxes can be unlocked."""
+    position = 0
+    unlocked = {}
 
-    while stack:
-        box = stack.pop()
-        for key in boxes[box]:
-            if not unlocked[key]:
-                unlocked[key] = True
-                stack.append(key)
-
-    return all(unlocked)
+    for box in boxes:
+        if len(box) == 0 or position == 0:
+            unlocked[position] = "always_unlocked"
+        for key in box:
+            if key < len(boxes) and key != position:
+                unlocked[key] = key
+        if len(unlocked) == len(boxes):
+            return True
+        position += 1
+    return False
